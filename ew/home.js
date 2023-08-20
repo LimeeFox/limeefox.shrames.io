fba7f9cceb6c8a7aa925d0b7d2373b7ad9345b17
LimeeFox <limeefox@gmail.com>
2023-08-20 09:54:37 +0200

    lazy loading test

--- ew/home.js
+++ ew/home.js
@@ -21,7 +21,58 @@ window.addEventListener("scroll", function() {
     logo.style.opacity = opacity;
 });
 */
+function lazyLoadMedia() {
+    var images = document.querySelectorAll('img[data-src]');
+    images.forEach(function(image) {
+      var rect = image.getBoundingClientRect();
+      if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+        var src = image.getAttribute('data-src');
+        if (src) {
+          image.src = src;
+          image.removeAttribute('data-src');
+        }
+      }
+    });
 
+    // Lazy load iframes
+    var iframes = document.querySelectorAll('iframe[data-src]');
+    iframes.forEach(function(iframe) {
+        var rect = iframe.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+        var src = iframe.getAttribute('data-src');
+        if (src) {
+            iframe.src = src;
+            iframe.removeAttribute('data-src');
+      }
+    }
+  });
+  }
+
+var totalSlides = document.querySelectorAll('.background_slide').length;
+var currentSlideIndex = 0;
+var slides = document.querySelectorAll('.background_slide[data-bg-url]');
+
+function lazyLoadBackgrounds() {
+    // Load current slide
+    var currentSlide = slides[currentSlideIndex];
+    var bgUrl = currentSlide.getAttribute('data-bg-url');
+    if (bgUrl) {
+        currentSlide.style.backgroundImage = `url('${bgUrl}')`;
+        currentSlide.removeAttribute('data-bg-url');
+    }
+
+    // Preload next slide
+    var nextSlideIndex = (currentSlideIndex + 1) % totalSlides;
+    var nextSlide = slides[nextSlideIndex];
+    var nextBgUrl = nextSlide.getAttribute('data-bg-url');
+    if (nextBgUrl) {
+        nextSlide.style.backgroundImage = `url('${nextBgUrl}')`;
+        nextSlide.removeAttribute('data-bg-url');
+    }
+
+    // Update current slide index
+    currentSlideIndex = nextSlideIndex;
+}
 
 window.addEventListener("scroll", function() {
   var scrollPosition = window.pageYOffset;
@@ -51,6 +102,7 @@ window.addEventListener("scroll", function() {
   }
 
   logo.style.opacity = opacity;
+  lazyLoadMedia();
 });
 
 
@@ -112,3 +164,31 @@ $(document).ready(function() {
   $slickEl.slick('slickGoTo', 0, true);
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    let alertVisible = false;
+
+    document.getElementById("copy-button").addEventListener("click", function() {
+        const textToCopy = "mc.empirewar.org";
+        navigator.clipboard.writeText(textToCopy);
+        /*
+        navigator.clipboard.writeText(textToCopy).then(function() {
+            const alertDiv = document.getElementById("alert-success");
+            alertDiv.style.visibility = "visible";
+            alertVisible = true;
+
+            setTimeout(function() {
+                alertDiv.style.visibility = "hidden";
+                alertVisible = false;
+            }, 3000); // Hide after 3000 milliseconds (3 seconds)
+        }).catch(function(err) {
+            console.error("Could not copy text: ", err);
+        });
+        */
+    });
+});
+  
+
+lazyLoadBackgrounds();
+
+setInterval(lazyLoadBackgrounds, 3000);
